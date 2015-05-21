1ea067639340f4e592da8f94e96337b923b69924
ytshao <ytshao02@hotmail.com>
2015-05-20 23:56:13 -0500

    first commit

--- Restaurants.py
+++ Restaurants.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 app.config.from_pyfile('Config.py')
 
-# Here we are making our database!
+# Database component
+
 db = SQLAlchemy(app)
 
 class Restaurants(db.Model):
@@ -40,7 +41,9 @@ class Restaurants(db.Model):
         self.Phone = Phone
         self.Rest_key = Rest_key
         
-
+    def to_dict(self):
+        # return {"Name":self.Name, "City":self.City}
+        return {"Name":self.Name, "City":self.City, "Neighborhood":self.Neighborhood, "Latitude":self.Latitude, "Longitude":self.Longitude, "Rating":self.Rating,"Review_Count":self.Review_Count, "Phone":self.Phone, "Rest_key":self.Rest_key}
 
 class Users(db.Model):
     id = db.Column(db.Integer)
@@ -65,6 +68,9 @@ class Users(db.Model):
         self.Give_current = Give_current
         self.Receive_current = Receive_current
 
+    def to_dict(self):
+        return {"Lastname":self.Lastname, "Firstname":self.Firstname, "Facebook_id":self.Facebook_id, "Credits":self.Credits, "Scores":self.Scores, "Number_of_scores":self.Number_of_scores, "Number_of_runs":self.Number_of_runs, "Give_current":self.Give_current, "Receive_current":self.Receive_current}
+    
 class Auctionhouse(db.Model):
     id = db.Column(db.Integer)
     Restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
@@ -78,24 +84,54 @@ class Auctionhouse(db.Model):
         self.slot_time = slot_time
         self.Rest_slot = Rest_slot
 
+    def to_dict(self):
+        return {"Restaurant_id":self.Restaurant_id, "date":self.date, "slot_time":self.slot_time, "Rest_slot":self.Rest_slot}
 
-@app.route('/rest/findbyid/<int:id>/', methods = ['GET'])
+
+# API Functions
+@app.route('/restaurants/findbyid/<int:id>/', methods = ['GET'])
 def get_rest(id):    
-    query = Restaurants.query.filter_by(id=id).first()
-    return jsonify({'stuff':query})
-    # return jsonify({'restaurant_name':query.Name, 'restaurant_address':query.Address, 'phone':query.Phone})
+    query = Restaurants.query.filter_by(id=id).all()
+    return json.dumps([obj.to_dict() for obj in query])
 
-@app.route('/rest/', methods = ['GET'])
-def get_all():
-    return jsonify({'restaurants': Restaurants.query.all()})
 
-@app.route('/rest/', methods = ['POST'])
+@app.route('/restaurants/findbyname/<name>/', methods = ['GET'])
+def get_rest_name(name):
+    query = Restaurants.query.filter_by(Name=name).all()
+    return json.dumps([obj.to_dict() for obj in query])
+
+
+@app.route('/users/findbyid/<facebook_id>/', methods= ['GET'])
+def get_user(facebook_id):
+    query = Users.query.filter_by(Facebook_id=facebook_id).all()
+    return json.dumps([obj.to_dict() for obj in query])
+
+
+@app.route('/restaurants/', methods = ['GET'])
+def get_rest_all():
+    query = Restaurants.query.all()
+    return json.dumps([obj.to_dict() for obj in query])
+
+
+@app.route('/users/', methods = ['GET'])
+def get_user_all():
+    query = Users.query.all()
+    return json.dumps([obj.to_dict() for obj in query])
+
+
+@app.route('/auctionhouse/', methods = ['GET'])
+def get_auctionhouse_all():
+    query = Auctionhouse.query.all()
+    return json.dumps([obj.to_dict() for obj in query])
+
+
+@app.route('/restaurants/posts/', methods = ['POST'])
 def create_rest():
     #error check
-    if not request.json or not 'name' in request.json:
+    if not request.json or not 'Name' in request.json:
         abort(400)
 
-    rest = Restaurant(request.json.name, request.json.get('visitDate',''), request.json.get('reviews',''))
+    rest = Restaurant(request.json.get('Name',''), request.json.get('City',''), request.json.get('Neighborhood',''), request.json.get('Address',''), request.json.get('Picture_url',''), request.json.get('Latitude',''), request.json.get('Longitude',''), request.json.get('Rating',''), request.json.get('Review_Count',''), request.json.get('Phone',''), request.json.get('Rest_key',''))
 
     db.session.add(rest)
     db.session.commit()
@@ -103,7 +139,7 @@ def create_rest():
     return jsonify({'restaurant': rest}), 201
 
 
-@app.route('/rest/<int:id>', methods = ['DELETE'])
+@app.route('/restaurants/<int:id>', methods = ['DELETE'])
 def delete_rest(id):
     
     db.session.delete(Restaurants.query.get(id))
@@ -112,17 +148,25 @@ def delete_rest(id):
     return jsonify({'result': True})
 
 
-@app.route('/rest/<int:id>', methods = ['PUT'])
+@app.route('/restaurants/put/<int:id>', methods = ['PUT'])
 def update_rest(id):
     rest = Restaurants.query.get(id)
 
-    rest.name = request.json.get('name', rest.name)
-    rest.visitDate = request.json.get('visitDate', rest.visitDate)
-    rest.reviews = request.json.get('reviews', rest.reviews)
+    rest.Name = request.json.get('Name', rest.Name)
+    rest.City = request.json.get('City', rest.City)
+    rest.Neighborhood = request.json.get('Neighborhood', rest.Neighborhood)
+    rest.Address = request.json.get('Address', rest.Address)
+    rest.Picture_url = request.json.get('Picture_url', rest.Picture_url)
+    rest.Latitude = request.json.get('Latitude', rest.Latitude)
+    rest.Longitude = request.json.get('Longitude', rest.Longitude)
+    rest.Rating = request.json.get('Rating', rest.Rating)
+    rest.Review_Count = request.json.get('Review_Count', rest.Review_Count)
+    rest.Phone = request.json.get('Phone', rest.Phone)
+    rest.Rest_key = request.json.get('Rest_key'. rest.Rest_key)
 
     db.session.commit()
 
-    return jsonify( { 'rest': rest})
+    return json.dumps(rest.to_dict())
 
 
 if __name__ == '__main__':
